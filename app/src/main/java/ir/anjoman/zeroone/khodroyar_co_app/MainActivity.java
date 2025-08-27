60bb229850637f966edc81f421a08cd80620d519
devmjyo <mohammad1387y@gmail.com>
2025-08-27 04:34:31 -0700

    test

---
+++ app/src/main/java/ir/anjoman/zeroone/khodroyar_co_app/MainActivity.java
@@ -0,0 +1,14 @@
+package ir.anjoman.zeroone.khodroyar_co_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
